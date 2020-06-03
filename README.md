4426105ea904f2e57412e335d6b7b478161f0cbe
sunjianguo <jacky.sun@hotpepperusa.com>
2020-06-03 14:58:33 +0800

    Add udev/samba etc

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # ubuntu
 
 This repository is for collecting the utils for ubuntu series.
+
+# Installation
+
+## udev
+sudo usermod -aG plugdev $(whoami)
+sudo cp --parents etc/udev/rules.d/51-android.rules /
+sudo /etc/init.d/udev restart
+
+## samba
+sudo apt install samba
+sudo cp --parents etc/samba/smb.conf /
+sudo /etc/init.d/smbd restart
---
+++ post_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo sed -i 's/us.archive.ubuntu/mirrors.aliyun/g' /etc/apt/sources.list
+sudo sed -i 's/cn.archive.ubuntu/mirrors.aliyun/g' /etc/apt/sources.list
+
+sudo apt update
+
+sudo apt-get -y install openjdk-8-jdk
+
+#utils
+sudo apt-get install -y vim ssh samba
+
+#Android
+sudo apt-get install -y libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib 
+sudo apt-get install -y git flex bison gperf build-essential libncurses5-dev:i386 
+sudo apt-get install -y tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386 
+sudo apt-get install -y dpkg-dev libsdl1.2-dev libesd0-dev
+sudo apt-get install -y git-core gnupg flex bison gperf build-essential  
+sudo apt-get install -y zip curl zlib1g-dev gcc-multilib g++-multilib 
+sudo apt-get install -y libc6-dev-i386 
+sudo apt-get install -y lib32ncurses5-dev x11proto-core-dev libx11-dev 
+sudo apt-get install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
+sudo apt-get install -y lib32z-dev ccache
+#for MTK
+sudo apt-get install -y libssl-dev libswitch-perl
+
